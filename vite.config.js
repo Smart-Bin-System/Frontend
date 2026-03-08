ed9afb6a4a7e4f4ef20f3df31c279613aad71368
Mihashi Ranasinghe <mihashiranasinghe18@gmail.com>
2026-03-08 23:11:00 +0530

    Project Architecture Setup
    - Create base folder structures and configuration files.
    - Complete architecture setup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
